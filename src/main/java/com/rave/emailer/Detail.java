c4bb1426e0ac4fac883c43d35547d741ca5bde5f
Daniel Nwankwo <dan_nwankwo@yahoo.co.uk>
2017-06-23 13:50:35 +0100

    first commit

---
+++ src/main/java/com/rave/emailer/Detail.java
@@ -0,0 +1,11 @@
+package com.rave.emailer;
+
+/**
+ * Created by Useradmin on 23/06/2017.
+ */
+public class Detail {
+    @Override
+    public String toString() {
+        return "Detail{}";
+    }
+}
---
+++ src/main/java/com/rave/emailer/EmailTest.java
@@ -0,0 +1,61 @@
+package com.rave.emailer;
+
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Useradmin on 22/06/2017.
+ */
+public class EmailTest {
+    public String sendEmail(ScheduledEvent scheduledEvent, Context context) {
+        List<String> destinationAddresses = new ArrayList<String>();
+        destinationAddresses.add("daniel.nwankwo74@gmail.com");
+        String from = "admin@nigeriachristian.com";
+        AmazonSimpleEmailService client = null;
+
+        /*subject*/
+        Content content = new Content();
+        String bodyMessage = "This is from a lambda function";
+        content.setData(bodyMessage);
+        Body body = new Body();
+        body.setHtml(content);
+
+        /*Body*/
+        Content subjectContent = new Content();
+        String subject = "Subject from lambda";
+        subjectContent.setData(subject);
+
+        /*Message*/
+        Message message = new Message();
+        message.setBody(body);
+        message.setSubject(subjectContent);
+
+        SendEmailRequest request = new SendEmailRequest().withSource(from)
+                .withDestination(new Destination(destinationAddresses))
+                .withMessage(message);
+
+        try {
+            System.out.println("Attempting to send an email through Amazon SES by using the AWS SDK for Java...");
+
+            if (client == null) {
+                client = AmazonSimpleEmailServiceClientBuilder.standard()
+                .build();
+            }
+
+            client.sendEmail(request); // this is where the exception is thrown
+            System.out.println("Email sent!");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.err.println(ex.getMessage());
+        }
+
+        return "done";
+    }
+}
